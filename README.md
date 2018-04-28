003760052664482395d0be497125724e11a82d1c
Alex Crichton <alex@alexcrichton.com>
2018-04-27 17:29:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# wbg-rand
+
+Implementation of `rand` for wasm32-unknown-unknown in Rust using
+`#[wasm_bindgen]`.
+
+## Usage
+
+First add a dependency to your Cargo.toml:
+
+```toml
+# Cargo.toml
+[dependencies]
+wbg-rand = "0.4"
+```
+
+Next add the following to your crate:
+
+```rust
+extern crate wbg_rand;
+
+use wbg_rand::{Rng, wasm_rand};
+```
+
+The `rand` crate is reexported from the `wbg-rand` crate so the `Rng` trait here
+is the [same as it is upstream](https://docs.rs/rand/0.4.2/rand/trait.Rng.html).
+
+And now you use `wasm_rand` just like you would `thread_rng`!
+
+# License
+
+This project is licensed under either of
+
+ * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+   http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or
+   http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in this project by you, as defined in the Apache-2.0 license,
+shall be dual licensed as above, without any additional terms or conditions.
---
+++ src/lib.rs
@@ -0,0 +1,77 @@
+//! A crate to enable usage of the `rand` crate on wasm32-unknown-unknown
+//!
+//! This crate provides two types, a `MathRandomRng` and a `WasmRng`. The
+//! `MathRandomRng` is a random number generated which is purely backed by
+//! `Math.random`. The `WasmRng` is a faster in-was RNG which draws entropy
+//! periodically from `Math.random`.
+//!
+//! Users of `rand::thread_rng` should feel right at home with `wasm_rng`.
+
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+extern crate wasm_bindgen;
+extern crate rand;
+#[macro_use]
+extern crate lazy_static;
+
+use std::sync::Mutex;
+use wasm_bindgen::prelude::*;
+
+pub use rand::*;
+
+/// A random number generated which is hooked up to `Math.random` in JS.
+pub struct MathRandomRng(());
+
+pub fn math_random_rng() -> MathRandomRng {
+    MathRandomRng(())
+}
+
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(js_namespace = Math)]
+    fn random() -> f64;
+}
+
+impl Rng for MathRandomRng {
+    fn next_u32(&mut self) -> u32 {
+        (random() * (u32::max_value() as f64)) as u32
+    }
+}
+
+/// A reasonably fast RNG for wasm which draws entropy from `Math.random`.
+pub struct WasmRng(());
+
+pub fn wasm_rng() -> WasmRng {
+    WasmRng(())
+}
+
+lazy_static! {
+    static ref RNG: Mutex<reseeding::ReseedingRng<IsaacRng, WasmRngReseed>> = {
+        Mutex::new(reseeding::ReseedingRng::new(math_random_rng().gen(), 32, WasmRngReseed))
+    };
+}
+
+struct WasmRngReseed;
+
+impl reseeding::Reseeder<IsaacRng> for WasmRngReseed {
+    fn reseed(&mut self, rng: &mut IsaacRng) {
+        *rng = math_random_rng().gen();
+    }
+}
+
+impl Rng for WasmRng {
+    fn next_u32(&mut self) -> u32 {
+        RNG.lock()
+            .unwrap_or_else(|e| e.into_inner())
+            .next_u32()
+    }
+    fn next_u64(&mut self) -> u64 {
+        RNG.lock()
+            .unwrap_or_else(|e| e.into_inner())
+            .next_u64()
+    }
+    fn fill_bytes(&mut self, bytes: &mut [u8]) {
+        RNG.lock()
+            .unwrap_or_else(|e| e.into_inner())
+            .fill_bytes(bytes)
+    }
+}
